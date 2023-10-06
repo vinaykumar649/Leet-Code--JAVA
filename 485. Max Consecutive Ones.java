9c375b1476fe028404ae6b18dfdfe075825bbd80
Vinay Kumar <92634975+vinaykumar649@users.noreply.github.com>
2023-10-06 14:42:29 +0530

    Add files via upload

---
+++ 485. Max Consecutive Ones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int res =0;
+        int count=0;
+        for(int i=0; i<nums.length; i++)  {
+            if(nums[i]==1){
+                count++;
+            }
+            else{
+                res = Math.max(res, count);
+                count =0;
+            }
+        }
+        return Math.max(res, count);
+    }
+}
