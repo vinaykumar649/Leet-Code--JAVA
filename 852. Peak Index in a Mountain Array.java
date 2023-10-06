9d6d011b1381de7dc2ec4eb2eaf5901f62aa9221
Vinay Kumar <92634975+vinaykumar649@users.noreply.github.com>
2023-10-06 14:43:25 +0530

    Add files via upload

---
+++ 852. Peak Index in a Mountain Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int i = 0;
+        int j = arr.length-1;
+        while(i<j){
+            int mid = i + (j-i)/2;
+            if(arr[mid]>arr[mid+1]){
+                j = mid;
+            }
+            else{
+                i = mid+1;
+            }
+        }
+        return i;
+    }
+}
